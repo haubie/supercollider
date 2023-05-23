f2e643b336f16e3f4a117eaa21ea4a293cd30763
David Haubenschild <haubie@gmail.com>
2023-05-23 23:08:56 +1000

    First package release

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule SuperCollider.MixProject do
   def project do
     [
       app: :supercollider,
+      name: "SuperCollider",
+      description: "An Elixir library for interacting with SuperCollider, an audio synthesis and composition platform.",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/haubie/supercollider"
     ]
   end
 
@@ -24,4 +28,19 @@ defmodule SuperCollider.MixProject do
       {:osc, "~> 0.1.2"}
     ]
   end
+
+  defp package() do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/haubie/supercollider"},
+      maintainers: ["David Haubenschild"]
+    ]
+  end
+
 end
